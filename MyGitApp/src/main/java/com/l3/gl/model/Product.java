95927120115488812c079113364bb2b7a93947f1
chaima <chaimabouketta30@gmail.com>
2025-12-27 11:25:11 +0100

    Add Ui class in view and User/Product classes in model

---
+++ MyGitApp/src/main/java/com/l3/gl/model/Product.java
@@ -0,0 +1,10 @@
+package com.l3.gl.model; 
+public class Product { 
+private String name; 
+private double price; 
+public Product(String name, double price) { 
+this.name = name; 
+this.price = price; 
+} 
+// Getters and setters 
+}

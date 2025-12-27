08c0b61252e9d548f3d31d4c0643398ec000e5b5
chaimabouketta30-hash <chaimabouketta30@gmail.com>
2025-12-27 19:19:32 +0100

    Refactor Controller to handle user input and display
    Refactored Controller class to use Ui for user input and display messages.

--- MyGitApp/src/main/java/com/l3/gl/controller/Controller.java
+++ MyGitApp/src/main/java/com/l3/gl/controller/Controller.java
@@ -1,16 +1,45 @@
-package com.l3.gl.controller; 
-import com.l3.gl.model.Product; 
-import com.l3.gl.model.User; 
-import com.l3.gl.view.Ui; 
-public class Controller { 
-   private Ui view; 
-   private Product product; 
-   private User user; 
-   public Controller(Product product, User user) { 
-      this.product = product; 
-      this.user = user; 
-   }  
-   public String sayHello() { 
-      return "hello from controller"; 
-   }
-   private void showDetails() { // your work } } 
+package com.l3.gl.controller;
+
+import com.l3.gl.model.User;
+import com.l3.gl.model.Product;
+import com.l3.gl.view.Ui;
+
+import javax.swing.*;
+
+public class Controller {
+
+    private Ui view;
+
+    public Controller(Ui view) {
+        this.view = view;
+
+        view.getSubmitButton().addActionListener(e -> {
+            try {
+                User user = new User(
+                        view.getUserName(),
+                        view.getUserEmail()
+                );
+
+                Product product = new Product(
+                        view.getProductName(),
+                        view.getProductPrice()
+                );
+
+                JOptionPane.showMessageDialog(null,
+                        "User Information:\n" +
+                        "Name: " + user.getName() + "\n" +
+                        "Email: " + user.getEmail() + "\n\n" +
+                        "Product Information:\n" +
+                        "Name: " + product.getName() + "\n" +
+                        "Price: " + product.getPrice()
+                );
+
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null,
+                        "Please enter valid data.",
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        });
+    }
+}
